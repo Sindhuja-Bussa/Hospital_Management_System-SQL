a547b7512e8537dbf8a8235f314667446f0ba664
Sindhuja Bussa <sindhubussa03@gmail.com>
2025-02-10 20:44:21 +0530

    Update 05_create_medical_records_table.sql

--- 05_create_medical_records_table.sql
+++ 05_create_medical_records_table.sql
@@ -1,13 +1,11 @@
-CREATE TABLE MedicalRecords (
-    record_id INT AUTO_INCREMENT,
+CREATE TABLE MedicalRecords(
+    record_id INT PRIMARY KEY,
     patient_id INT,
     doctor_id INT,
     visit_date DATE,
-    diagnosis VARCHAR(255),
-    prescribed_medication VARCHAR(255),
-    notes TEXT,
-    PRIMARY KEY (record_id),
+    diagnosis VARCHAR(250),
+    prescribed_medication VARCHAR(250),
+    notes varchar(500),
     FOREIGN KEY (patient_id) REFERENCES Patients(patient_id),
     FOREIGN KEY (doctor_id) REFERENCES Doctors(doctor_id)
-);
-
+    );
