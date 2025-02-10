b892991a74c60a51623fe3659e262a1e207923d8
Sindhuja Bussa <sindhubussa03@gmail.com>
2025-02-10 20:42:01 +0530

    Update 02_create_doctors_table.sql

--- 02_create_doctors_table.sql
+++ 02_create_doctors_table.sql
@@ -1,10 +1,9 @@
-CREATE TABLE Doctors (
-    doctor_id INT AUTO_INCREMENT,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    specialty VARCHAR(100),
+CREATE TABLE Doctors(
+    doctor_id INT PRIMARY KEY, 
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    speciality VARCHAR(100),
     contact_info VARCHAR(100),
-    availability VARCHAR(50),
-    PRIMARY KEY (doctor_id)
-);
+    availability VARCHAR(50)
+    );
 
